25094ff13de7045b49c5f0b27673baed6057eb58
david042 <david.mal04@hotmail.com>
2021-05-23 01:49:10 +0900

    agora tira foto e salva

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,17 @@
-// This is a JavaScript file
+$(document).on("click", "#camera", function(){
+  navigator.camera.getPicture(onSuccess, onFail, {
+    quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    correctOrientation: true,
+    saveToPhotoAlbum: true
+  });
 
+  function onSuccess(imageURI) {
+    var image = document.getElementById('imagem');
+    image.src = imageURI;
+  }
+
+  function onFail(message) {
+    alert('Failed because: ' + message);
+  }
+}); 
