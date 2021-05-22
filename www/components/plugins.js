d28475235ae7d27a2707da62bd22421240c1568b
david042 <david.mal04@hotmail.com>
2021-05-23 03:13:00 +0900

    agora mostra o tipo de conexão

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -12,20 +12,20 @@ $(document).on("click", "#camera", function(){
   }
 
   function onFail(message) {
-    alert('Failed because: ' + message);
+    alert('Falhou porque: ' + message);
   }
 });
 
 $(document).on("click", "#codigo", function(){
   cordova.plugins.barcodeScanner.scan(
     function (result) {
-      alert("We got a barcode\n" +
-            "Result: " + result.text + "\n" +
-            "Format: " + result.format + "\n" +
-            "Cancelled: " + result.cancelled);
+      alert("Habēmus Cōdicem Barrae \n" +
+            "Resultado: " + result.text + "\n" +
+            "Formato: " + result.format + "\n" +
+            "Cancelado: " + result.cancelled);
     },
     function (error) {
-      alert("Scanning failed: " + error);
+      alert("O escaneamento falhou: " + error);
     },
     {
       preferFrontCamera : false, // iOS and Android
@@ -33,12 +33,32 @@ $(document).on("click", "#codigo", function(){
       showTorchButton : true, // iOS and Android
       torchOn: true, // Android, launch with the torch switched on (if available)
       saveHistory: true, // Android, save scan history (default false)
-      prompt : "Place a barcode inside the scan area", // Android
+      prompt : "Aponte a um Código de barras ou QR Code", // Android
       resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
       formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
-      orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+      orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
       disableAnimations : true, // iOS
       disableSuccessBeep: false // iOS and Android
     }
   );
 });
+
+function testarConexao() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Conexão Indefinida';
+    states[Connection.ETHERNET] = 'Conexão Ethernet';
+    states[Connection.WIFI]     = 'Conexão WiFi';
+    states[Connection.CELL_2G]  = 'Conexão 2G';
+    states[Connection.CELL_3G]  = 'Conexão 3G';
+    states[Connection.CELL_4G]  = 'Conexão 4G';
+    states[Connection.CELL]     = 'Conexão Genérica';
+    states[Connection.NONE]     = 'Sem Conexão';
+
+    alert('Tipo de conexão: ' + states[networkState]);
+}
+
+$(document).on("click", "#conexao", function(){
+  testarConexao();
+})
