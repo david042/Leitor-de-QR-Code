78da54a2b38863e697597f253d8265211a0571ef
david042 <david.mal04@hotmail.com>
2021-05-23 02:13:52 +0900

    agora lê código de barras

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -14,4 +14,31 @@ $(document).on("click", "#camera", function(){
   function onFail(message) {
     alert('Failed because: ' + message);
   }
-}); 
+});
+
+$(document).on("click", "#codigo", function(){
+  cordova.plugins.barcodeScanner.scan(
+    function (result) {
+      alert("We got a barcode\n" +
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
+            "Cancelled: " + result.cancelled);
+    },
+    function (error) {
+      alert("Scanning failed: " + error);
+    },
+    {
+      preferFrontCamera : false, // iOS and Android
+      showFlipCameraButton : true, // iOS and Android
+      showTorchButton : true, // iOS and Android
+      torchOn: true, // Android, launch with the torch switched on (if available)
+      saveHistory: true, // Android, save scan history (default false)
+      prompt : "Place a barcode inside the scan area", // Android
+      resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+      formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+      orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+      disableAnimations : true, // iOS
+      disableSuccessBeep: false // iOS and Android
+    }
+  );
+});
